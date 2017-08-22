077c8df20c8bf71b616a39d807eaef42492b9fde
Raz1789 <navaneeth_3@hotmail.com>
2017-08-22 21:17:41 +0400

    Initial Commit
    Initial Commit

---
+++ GameProject/Burger.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameProject
+{
+    /// <summary>
+    /// A burger
+    /// </summary>
+    public class Burger
+    {
+        #region Fields
+
+        // graphic and drawing info
+        Texture2D sprite;
+        Rectangle drawRectangle;
+
+        // burger stats
+        int health = 100;
+
+        // shooting support
+        bool canShoot = true;
+        int elapsedCooldownMilliseconds = 0;
+        Projectile projectile;
+
+        // sound effect
+        SoundEffect shootSound;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///  Constructs a burger
+        /// </summary>
+        /// <param name="contentManager">the content manager for loading content</param>
+        /// <param name="spriteName">the sprite name</param>
+        /// <param name="x">the x location of the center of the burger</param>
+        /// <param name="y">the y location of the center of the burger</param>
+        /// <param name="shootSound">the sound the burger plays when shooting</param>
+        public Burger(ContentManager contentManager, string spriteName, int x, int y,
+            SoundEffect shootSound)
+        {
+            LoadContent(contentManager, spriteName, x, y);
+            this.shootSound = shootSound;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the collision rectangle for the burger
+        /// </summary>
+        public Rectangle CollisionRectangle
+        {
+            get { return drawRectangle; }
+        }
+
+        /// <summary>
+        /// Gets and sets the health of the Burger
+        /// </summary>
+        public int Health
+        {
+            get { return health; }
+            set {
+                health = value;
+                if (health < 0) health = 0;
+                }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Updates the burger's location based on mouse. Also fires 
+        /// french fries as appropriate
+        /// </summary>
+        /// <param name="gameTime">game time</param>
+        /// <param name="keyBoard">the current state of the mouse</param>
+        public void Update(GameTime gameTime, KeyboardState keyBoard)
+        {
+            // burger should only respond to input if it still has health
+            if (health > 0)
+            {
+
+                // move burger using mouse
+                //drawRectangle.X = keyBoard.X - (drawRectangle.Width / 2);
+                //drawRectangle.Y = keyBoard.Y - (drawRectangle.Height / 2);
+
+                // move burger using Keyboard
+                if (keyBoard.IsKeyDown(Keys.W)) drawRectangle.Y -= GameConstants.BurgerMovementAmount;
+                if (keyBoard.IsKeyDown(Keys.S)) drawRectangle.Y += GameConstants.BurgerMovementAmount;
+                if (keyBoard.IsKeyDown(Keys.A)) drawRectangle.X -= GameConstants.BurgerMovementAmount;
+                if (keyBoard.IsKeyDown(Keys.D)) drawRectangle.X += GameConstants.BurgerMovementAmount;
+
+
+                // clamp burger in window
+                //Right Clamp
+                if (drawRectangle.Right > GameConstants.WindowWidth)
+                    drawRectangle.X = GameConstants.WindowWidth - drawRectangle.Width;
+
+                //Left Clamp
+                if (drawRectangle.Left < 0)
+                    drawRectangle.X = 0;
+
+                //Bottom Clamp
+                if (drawRectangle.Bottom > GameConstants.WindowHeight)
+                    drawRectangle.Y = GameConstants.WindowHeight - drawRectangle.Height;
+
+                //Top Clamp
+                if (drawRectangle.Top < 0)
+                    drawRectangle.Y = 0;
+
+                // update shooting allowed
+                if (keyBoard.IsKeyDown(Keys.Space) && canShoot)
+                {
+                    canShoot = false;
+                    //Projectile initialization
+                    projectile = new Projectile(ProjectileType.FrenchFries, Game1.GetProjectileSprite(
+                        ProjectileType.FrenchFries), drawRectangle.X + drawRectangle.Width / 2, 
+                        drawRectangle.Y - GameConstants.FrenchFriesProjectileOffset, 
+                        GameConstants.FrenchFriesProjectileSpeed);
+
+                    //Adding Projectile
+                    Game1.AddProjectile(projectile);
+
+                    //play shoot soundEffects
+                    shootSound.Play();
+                }
+
+                // timer concept (for animations) introduced in Chapter 7
+                if(!canShoot)
+                {
+                    elapsedCooldownMilliseconds += gameTime.ElapsedGameTime.Milliseconds;
+                }
+                // shoot if appropriate
+                if(elapsedCooldownMilliseconds >= GameConstants.BurgerTotalCooldownMilliseconds || keyBoard.IsKeyUp(Keys.Space))
+                {
+                    elapsedCooldownMilliseconds = 0;
+                    canShoot = true;
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// Draws the burger
+        /// </summary>
+        /// <param name="spriteBatch">the sprite batch to use</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(sprite, drawRectangle, Color.White);
+
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Loads the content for the burger
+        /// </summary>
+        /// <param name="contentManager">the content manager to use</param>
+        /// <param name="spriteName">the name of the sprite for the burger</param>
+        /// <param name="x">the x location of the center of the burger</param>
+        /// <param name="y">the y location of the center of the burger</param>
+        private void LoadContent(ContentManager contentManager, string spriteName,
+            int x, int y)
+        {
+            // load content and set remainder of draw rectangle
+            sprite = contentManager.Load<Texture2D>(spriteName);
+            drawRectangle = new Rectangle(x - sprite.Width / 2,
+                y - sprite.Height / 2, sprite.Width,
+                sprite.Height);
+        }
+
+        #endregion
+    }
+}
